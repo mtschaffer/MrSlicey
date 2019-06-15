f563de52844a425549a0cb952e74ec0f781e692b
Brandon Stansbury <brandon.stansbury@system1.com>
2019-06-15 13:54:45 -0700

    add evil turkey leg

---
+++ characters/turkeyleg.py
@@ -0,0 +1,31 @@
+import os
+
+import pygame
+
+from utils.fg_element import FGElement
+
+
+IMAGE_PATH = os.path.join('images', 'turkeyleg.png')
+IMAGE = pygame.image.load(IMAGE_PATH).convert_alpha()
+
+
+class TurkeyLeg(FGElement):
+    def __init__(self, x, y, angle, rotational_velocity):
+        super().__init__(
+            image=IMAGE,
+            x=x,
+            y=y,
+            angle=angle,
+            velocity=0,
+            move_x=0,
+            move_y=0
+        )
+        self.rotational_velocity = rotational_velocity
+
+    def update(self, model, lag_scalar):
+        self.angle += (self.rotational_velocity * lag_scalar)
+
+    def draw(self, screen):
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
+        w, h = rotated_image.get_size()
+        screen.blit(rotated_image, (int(self.x - w / 2), int(self.y - h / 2)))
